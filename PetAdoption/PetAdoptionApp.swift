65b978700b26d451ad1337af653e5fb74e4fbaf3
Marasy Phi <hipporasy@gmail.com>
2021-01-17 15:06:24 +0700

    Initial Commit

---
+++ PetAdoption/PetAdoptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAdoptionApp.swift
+//  PetAdoption
+//
+//  Created by Marasy Phi on 17/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PetAdoptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
